fb62b6b02907fee52af6899fd524cf1bb2553e62
vlveqoo <vlveqoo@gmail.com>
2024-03-10 19:01:44 +0900

    3주차 과제 제출

--- homeworks/5_advanced.js
+++ homeworks/5_advanced.js
@@ -20,7 +20,7 @@
         puzzle[sayHello(2)] = 2;
 
         const result = puzzle;
-        const guess = 바코;
+        const guess = {3:1, 4:2};
 
         expect(result).to.eql(guess);
       });
@@ -49,7 +49,7 @@
         }
 
         const result = getPassedSubjects(student);
-        const guess = 바코;
+        const guess = ['science', 'history'];
 
         expect(result).to.eql(guess);
       });
@@ -70,7 +70,8 @@
         const functionArray = [double, square, subtractFive];
 
         const result = functionArray[2](functionArray[1](3));
-        const guess = 바코;
+        //subtractFive(square(3))
+        const guess = 4;
 
         expect(result).to.eql(guess);
       });
@@ -98,7 +99,7 @@
         }
 
         const result = arr[2].job;
-        const guess = 바코;
+        const guess = "designer";
 
         expect(result).to.eql(guess);
       });
@@ -133,7 +134,7 @@
         };
 
         const result = calculateTotalProfit(account);
-        const guess = 바코;
+        const guess = 900;
 
         expect(result).to.eql(guess);
       });
@@ -149,7 +150,7 @@
 
         result.splice(midIndex, 1);
 
-        const guess = 바코;
+        const guess = [1, 3];
 
         expect(result).to.eql(guess);
       });
@@ -164,7 +165,7 @@
           }
         }
 
-        const guess = 바코;
+        const guess = 9;
 
         expect(result).to.eql(guess);
       });
@@ -183,7 +184,7 @@
         }
 
         const result = reverseArr(arr);
-        const guess = 바코;
+        const guess = ["o", "l", "l", "e", "h"];
 
         expect(result).to.eql(guess);
       });
@@ -198,7 +199,7 @@
           }
         }
 
-        const guess = 바코;
+        const guess = [1, 5, 9, 15];
 
         expect(result).to.eql(guess);
       });
@@ -213,7 +214,7 @@
           }
         }
 
-        const guess = 바코;
+        const guess = [15, 9, 5, 1];
 
         expect(result).to.eql(guess);
       });
@@ -227,7 +228,22 @@
          */
         function indexOf(list, target) {
           // [시작] 여러분의 로직을 아래에 작성해주세요.
-          return 0;
+          let targetLocationArr = [];
+          let targetLocation;
+
+          for(let i in list){
+            if(list[i] === target){
+              targetLocationArr.push(i);
+            }
+          }
+
+          if(targetLocationArr.length>0){
+            targetLocation = Number(targetLocationArr[0]);
+          } else{
+            targetLocation = -1;
+          }
+
+          return targetLocation
           // [끝] 여러분의 로직을 위에 작성해주세요.
         }
 
